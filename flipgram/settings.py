d3cd27375eae1318e14e6be038574fd20685bfe7
Alex Kiprop <kipropalex59@gmail.com>
2020-02-10 10:46:50 +0300

    config the settings

--- flipgram/settings.py
+++ flipgram/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'flipgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2,
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'instagram',
         'USER': 'alex',
         'PASSWORD': 'lexy',
