058b7f3ffab03461fbddeb821ef89a69d13dd52b
Alex Kiprop <kipropalex59@gmail.com>
2020-02-10 09:01:04 +0300

    add a class to indicate the category of the image

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
 
-# Create your models here.
+
+class Tag(models.Model):
+    """ class to indicate the category of the image"""
+    name = models.CharField(max_length=30)
+
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to='profiles/', null=True)
+    bio = models.TextField(null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        '''
+        Display for profiles in profile table
+        '''
+        return self.user.username
